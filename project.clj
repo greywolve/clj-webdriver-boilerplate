b75f079b047148875652da823d5d12b8ac4abe10
greywolve <oliverpowell84@gmail.com>
2013-02-17 13:39:30 +0200

    Update project.clj

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "clj-web-driver-boilerplate" 
   :url "http://www.github.com/greywolve/clj-webdriver-boilerplate"
   :license {:name "MIT License"
-            :url ""}
+            :url "https://github.com/greywolve/clj-webdriver-boilerplate/blob/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.5.0-RC16"]
                  [clj-webdriver "0.6.0-beta2"]]
   :eval-in :leiningen)
