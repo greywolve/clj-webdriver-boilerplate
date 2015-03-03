94307ecba63d334080eb5f0994672efb92b29934
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2015-03-03 21:55:52 +0100

    Explicitly install new Selenium

--- project.clj
+++ project.clj
@@ -4,5 +4,7 @@
   :license {:name "MIT License"
             :url "https://github.com/greywolve/clj-webdriver-boilerplate/blob/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clj-webdriver "0.6.1"]]
+                 [clj-webdriver "0.6.1"
+                  :exclusions [org.seleniumhq.selenium/selenium-server]]
+                 [org.seleniumhq.selenium/selenium-server "2.45.0"]]
   :eval-in :leiningen)
