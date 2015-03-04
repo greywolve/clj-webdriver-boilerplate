e49d35437b4fe98ca6ea8f607f5953a37d9712ee
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2015-03-04 21:18:09 +0100

    Bump version to 0.6.1
    Plan is to match clj-webdriver version.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clj-webdriver-boilerplate "0.1.0-SNAPSHOT"
+(defproject clj-webdriver-boilerplate "0.6.1"
   :description "clj-web-driver-boilerplate" 
-  :url "http://www.github.com/greywolve/clj-webdriver-boilerplate"
+  :url "https://www.github.com/greywolve/clj-webdriver-boilerplate"
   :license {:name "MIT License"
             :url "https://github.com/greywolve/clj-webdriver-boilerplate/blob/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.5.1"]
